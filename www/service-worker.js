0392431fd031f0ce85f155b5745008c37224116b
baierschmidtja <john.a.baierschmidt@gmail.com>
2020-11-08 01:15:51 -0500

    Made LMCTFY a PWA (works offline)

---
+++ www/service-worker.js
@@ -0,0 +1,50 @@
+const CACHE = "lmctfy-cache";
+const PREVIOUS_CACHE = "lmctfy-cache-v1";
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.addAll(
+        [
+          'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css',
+          '/lmctfy.css',
+          '/lmctfy.js',
+          'https://lmctfy.baierschmidtja.com',
+          '/index.html'
+        ]
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames
+          .filter(function (cacheName) {
+            if (cacheName === PREVIOUS_CACHE) {
+              return true;
+            }
+          })
+          .map(function (cacheName) {
+            return caches.delete(cacheName);
+          }),
+      );
+    }),
+  );
+});
